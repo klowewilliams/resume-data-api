c89880906aa8dc22a3edd02ceae8fe2af75aa30e
Zach Bosteel <zbosteel@gmail.com>
2015-07-06 19:40:52 -0500

    Updated controller with restful actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,23 @@
 class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+  
+  def create
+    student = Student.create
+    redirect_to "/students/#{student.id}"
+  end
+
+  def show
+    @student = Student.find_by[params[:id]]
+  end
+
+  def update
+    redirect_to "/students/#{student.id}"
+  end
+
+  def destroy
+    redirect_to "/students"
+  end
+
 end
