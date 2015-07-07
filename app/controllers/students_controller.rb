44e17ae81c3e2d164f04a52a95505151b0706820
Zach Bosteel <zbosteel@gmail.com>
2015-07-06 20:40:20 -0500

    disabled protect from forgery to test from postman

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,7 @@ class StudentsController < ApplicationController
   end
   
   def create
-    student = Student.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin: params[:linkedin], twitter: params[:twitter], blog_site: params[:blog_site], resume: params[:resume], github: params[:github], photo: params[:photo])
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin: params[:linkedin], twitter: params[:twitter], blog_site: params[:blog_site], resume: params[:resume], github: params[:github], photo: params[:photo])
     render :show
   end
 
