3dee914330a42d19b2ed069a7348defa1cefe491
Zach Bosteel <zbosteel@gmail.com>
2015-07-06 20:53:59 -0500

    update and delete actions implemented

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,7 @@ class StudentsController < ApplicationController
   
   def create
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin: params[:linkedin], twitter: params[:twitter], blog_site: params[:blog_site], resume: params[:resume], github: params[:github], photo: params[:photo])
+
     render :show
   end
 
@@ -13,12 +14,16 @@ class StudentsController < ApplicationController
   end
 
   def update
+    @student = Student.find_by(id: params[:id])
+    @student.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin: params[:linkedin], twitter: params[:twitter], blog_site: params[:blog_site], resume: params[:resume], github: params[:github], photo: params[:photo])
+
 
     render :show
   end
 
   def destroy
-
+    @student = Student.find_by(id: params[:id])
+    @student.destroy
     render nil
   end
 
