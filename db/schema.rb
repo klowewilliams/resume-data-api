43731f5b3282324fefaadb160bc5e20c7a4ee875
Sam O'Connell <sjo773@gmail.com>
2015-07-06 21:23:56 -0500

    created education model

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,17 @@
 
 ActiveRecord::Schema.define(version: 20150707015125) do
 
+  create_table "educations", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string   "degree",          limit: 255
+    t.string   "university_name", limit: 255
+    t.text     "details",         limit: 65535
+    t.integer  "student_id",      limit: 4
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
   create_table "experiences", force: :cascade do |t|
     t.datetime "start_date"
     t.datetime "end_date"
